29e23d0fea84b1726a8104f610bb629944f4d66b
Sun_IT <daiphatle123@gmail.com>
2024-06-05 18:31:37 +0700

    complete Weather Forecast

--- Gemfile
+++ Gemfile
@@ -3,6 +3,9 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby "3.2.1"
 
+gem 'httparty'
+gem 'geocoder'
+
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.0.8", ">= 7.0.8.1"
 
---
+++ app/controllers/weather_forecasts_controller.rb
@@ -0,0 +1,29 @@
+class WeatherForecastsController < ApplicationController
+    def index
+        @city = params[:city] || 'HaNoi'
+        @weather = get_weather(@city)
+        if @weather.present?
+          # Các câu lệnh xử lý dữ liệu thời tiết ở đây
+        else
+          flash.now[:error] = "Failed to retrieve weather data for #{@city}"
+          render :index
+        end
+    end
+
+    private
+
+    def get_weather(city)
+        api_key = '4434526b73164a9097693313240506'
+        url = "https://api.weatherapi.com/v1/forecast.json?key=#{api_key}&q=#{city}&days=5"
+        response = HTTParty.get(url)
+
+        if response.success?
+            @weather_data = response.parsed_response
+        end
+    end
+
+    def get_current_location
+      location = Geocoder.search(request.remote_ip).first
+      return location.city
+    end
+end
